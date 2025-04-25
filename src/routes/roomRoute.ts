bc20cfd6389cc02a673b0a9a5568bcb79cb5c5bc
Alex-Br1s <alexbris21@gmail.com>
2025-04-25 16:00:06 -0300

    Endpoint de create room funcionando ahora si

--- src/routes/roomRoute.ts
+++ src/routes/roomRoute.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { handleCreateRoom } from "../handlers/roomHandler";
+import { authenticateToken } from "../middlewares/authenticateToken";
 
 const roomRoutes = Router();
 
-roomRoutes.post("/create-room", (req, res, next) => {
+roomRoutes.post("/create-room", authenticateToken, (req, res, next) => {
   handleCreateRoom(req, res, next).catch(next);
 });
 
